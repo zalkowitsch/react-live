e54a6a5c067cf86c76e995c150378df513464c19
Oskar Alves <osk.alves@gmail.com>
2021-05-06 14:38:10 -0300

    update version 1.0.2

--- deploy.js
+++ deploy.js
@@ -1,12 +1,19 @@
 const { execSync } = require('child_process');
 const { program } = require('commander');
 
+program
+  .description('Publish a version of Generator Dot on Github Packages')
+  .action(() => {
+    console.log('---------GITHUB_TOKEN----------');
+    console.log(process.env.GITHUB_TOKEN);
+    console.log('---------GITHUB_TOKEN----------');
 
-program.description('Publish a version of Generator Dot on Github Packages').action(() => {
-  if (!process.env.GITHUB_API_KEY) {
-    throw new Error('To release you need to set the GITHUB_API_KEY environment variable');
-  }
-  execSync('npm publish');
-});
+    if (!process.env.GITHUB_TOKEN) {
+      throw new Error(
+        'To release you need to set the GITHUB_TOKEN environment variable'
+      );
+    }
+    execSync('npm publish');
+  });
 
 program.parse(process.argv);
